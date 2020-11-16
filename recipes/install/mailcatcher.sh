4fcd5351e24e3628d873d54ad2f4b54205241e15
Lukáš Holeczy <info@lukasholeczy.eu>
2020-11-16 13:29:21 +0100

    Fix mailcatcher installation

--- recipes/install/mailcatcher.sh
+++ recipes/install/mailcatcher.sh
@@ -5,6 +5,8 @@ source ./core/brew.sh
 
 print_info "Installing Mailcatcher..."
 
+# Without this it's not working on Catalina and Big sur
+sudo gem install thin -v '1.5.1' -- --with-cflags="-Wno-error=implicit-function-declaration"
 sudo gem install mailcatcher > /dev/null 2>&1
 
 print_success "Done installing Mailcatcher!"
