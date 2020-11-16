51e878aa40d9de2f270242cf4679272148abddd8
Lukáš Holeczy <info@lukasholeczy.eu>
2020-11-16 09:39:49 +0100

    Fix iTerm2 config

--- recipes/config/iterm2.sh
+++ recipes/config/iterm2.sh
@@ -3,8 +3,13 @@
 source ./core/print.sh
 print_info "Configuring iTerm2..."
 
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/Documents/iterm"
+# Move config to config dir
 mv com.googlecode.iterm2.plist "$HOME/Documents/iterm/com.googlecode.iterm2.plist"
+# Enable config dir
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/Documents/iterm"
+# Do not show prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
 print_success "Done configuring iTerm2!"
 exit 0
