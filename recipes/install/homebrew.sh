c86449e9580ceddaacb51b128c018dc84b219ca7
Lukáš Holeczy <info@lukasholeczy.eu>
2020-11-16 09:30:43 +0100

    Fix installing homebrew

--- recipes/install/homebrew.sh
+++ recipes/install/homebrew.sh
@@ -11,9 +11,9 @@ fi
 echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" >/dev/null 2>&1
 echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.zshrc
 
-brew update
-brew upgrade
-brew cleanup
+brew update > /dev/null 2>&1
+brew upgrade > /dev/null 2>&1
+brew cleanup > /dev/null 2>&1
 
 print_success "Done installing homebrew!"
 exit 0
