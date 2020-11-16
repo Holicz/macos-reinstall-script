ffff5324192cd53e2c7d757fbb910a5c8b0aeafd
Lukáš Holeczy <info@lukasholeczy.eu>
2020-11-16 09:29:55 +0100

    Add config Safari recipe

---
+++ recipes/config/safari.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./core/print.sh
+print_info "Configuring Safari..."
+
+# Show full URL
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+# Do not open files after download
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+# Enable developer menu
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+# Disable auto-correct
+defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
+# Disable AutoFill
+defaults write com.apple.Safari AutoFillFromAddressBook -bool false
+defaults write com.apple.Safari AutoFillPasswords -bool false
+defaults write com.apple.Safari AutoFillCreditCardData -bool false
+defaults write com.apple.Safari AutoFillMiscellaneousForms -bool false
+
+print_success "Done configuring Safari!"
+exit 0
