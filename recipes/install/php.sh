b1d32d936d39f8aa7d7af8bf5db62d326cbba492
Lukáš Holeczy <info@lukasholeczy.eu>
2020-11-18 08:56:14 +0100

    Increase PHP memory limit

--- recipes/install/php.sh
+++ recipes/install/php.sh
@@ -11,6 +11,7 @@ brew_install php@7.4
 sed -i '' 's/;date.timezone =/date.timezone = Europe\/Prague/g' /usr/local/etc/php/7.4/php.ini
 sed -i '' 's/upload_max_filesize = 2M/upload_max_filesize = 2G/g' /usr/local/etc/php/7.4/php.ini
 sed -i '' 's/post_max_size = 8M/post_max_size = 2G/g' /usr/local/etc/php/7.4/php.ini
+sed -i '' 's/memory_limit = 128M/memory_limit = 2G/g' /usr/local/etc/php/7.4/php.ini
 
 print_success "Done installing PHP!"
 exit 0
